555b28a8b7d4acdab1ec3cf510f5eb3ed45ddaa5
alexis <alexiscapricornio14@gmail.com>
2026-05-17 20:50:37 -0300

    estructura creada

---
+++ Assets/ActivarSimulated.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class ActivarSimulated : MonoBehaviour
+{
+    [Header("Rigidbody")]
+    public Rigidbody2D rb;
+
+    [Header("Activar por tecla")]
+    public bool usarTecla = false;
+    public KeyCode tecla = KeyCode.E;
+
+    [Header("Activar por collider")]
+    public bool usarCollider = true;
+    public string tagActivador = "Player";
+
+    
+
+    void Update()
+    {
+        // Activar con tecla
+        if (usarTecla && Input.GetKeyDown(tecla))
+        {
+            ASimulated();
+        }
+    }
+
+    // Activar al tocar collider 2D
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!usarCollider) return;
+
+        if (other.CompareTag(tagActivador))
+        {
+            ASimulated();
+        }
+    }
+
+    public void ASimulated()
+    {
+        rb.simulated = true;
+    }
+
+    public void DeSimulated()
+    {
+        rb.simulated = false;
+    }
+}
---
+++ Assets/dialogo.cs
@@ -0,0 +1,96 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class dialogo : MonoBehaviour
+{
+    public TextMeshProUGUI texto;
+
+    [Header("Velocidad escritura")]
+    public float velocidadLetras = 0.05f;
+
+    [Header("Tiempo entre mensajes")]
+    public float tiempoEntreMensajes = 2f;
+
+    [Header("Control")]
+    public bool continuarDialogo = true;
+
+    private string[] mensajes =
+    {
+        "Bienvenido al juego",
+        "Sere tu guia",
+        "Para empezar presiona ENTER"
+    };
+
+    private int mensajeActual = 0;
+    private int letraActual = 0;
+
+    private float timerLetras = 0f;
+    private float timerMensaje = 0f;
+
+    private bool escribiendo = true;
+    private bool esperandoSiguiente = false;
+    private bool puedeEmpezar = false;
+
+    void Start()
+    {
+        texto.text = "";
+    }
+
+    void Update()
+    {
+        if (escribiendo)
+        {
+            timerLetras += Time.deltaTime;
+
+            if (timerLetras >= velocidadLetras)
+            {
+                timerLetras = 0f;
+
+                if (letraActual < mensajes[mensajeActual].Length)
+                {
+                    texto.text += mensajes[mensajeActual][letraActual];
+                    letraActual++;
+                }
+                else
+                {
+                    escribiendo = false;
+                    esperandoSiguiente = true;
+                    timerMensaje = 0f;
+                }
+            }
+        }
+
+        //ESPERAR ENTRE MENSAJES
+        else if (esperandoSiguiente)
+        {
+            if (!continuarDialogo)
+                return;
+
+            timerMensaje += Time.deltaTime;
+
+            if (timerMensaje >= tiempoEntreMensajes)
+            {
+                mensajeActual++;
+
+                if (mensajeActual < mensajes.Length)
+                {
+                    texto.text = "";
+                    letraActual = 0;
+
+                    escribiendo = true;
+                    esperandoSiguiente = false;
+                }
+                else
+                {
+                    esperandoSiguiente = false;
+                    puedeEmpezar = true;
+                }
+            }
+        }
+    }
+
+    public void controlDialogo()
+    {
+        continuarDialogo = !continuarDialogo;
+    }
+}
