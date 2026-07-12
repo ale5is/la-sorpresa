84cb05b5f5859f5fbbe31a38770ec00f513f4843
alexis <alexiscapricornio14@gmail.com>
2026-07-12 18:41:17 -0300

    ahora

--- Assets/scripts/dialogoFinal.cs
+++ Assets/scripts/dialogoFinal.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -8,62 +9,91 @@ public class dialogoFinal : MonoBehaviour
     [TextArea]
     public string mensaje;
 
-    public float tiempoEntrePalabras = 0.4f;
-    public float duracionFade = 0.3f;
+    public float tiempoEntreLetras = 0.03f;
+    public float duracionFade = 0.2f;
 
-    private string[] palabras;
-
-    private int palabraActual = 0;
-    private float timer = 0;
-
-    private bool haciendoFade = false;
-    private float fadeTimer = 0;
-
-    private string textoFijo = "";
-    private string palabraFade = "";
+    public GameObject objetoAlFinal;
 
     void Start()
     {
-        palabras = mensaje.Split(' ');
-        texto.text = "";
+        if (objetoAlFinal != null)
+            objetoAlFinal.SetActive(false);
+
+        StartCoroutine(Escribir());
     }
 
-    void Update()
+    IEnumerator Escribir()
     {
-        if (!haciendoFade)
+        texto.text = mensaje;
+        texto.ForceMeshUpdate();
+
+        TMP_TextInfo textInfo = texto.textInfo;
+
+        // Ocultar todas las letras
+        for (int i = 0; i < textInfo.characterCount; i++)
         {
-            timer += Time.deltaTime;
+            if (!textInfo.characterInfo[i].isVisible)
+                continue;
 
-            if (timer >= tiempoEntrePalabras && palabraActual < palabras.Length)
-            {
-                timer = 0;
+            int materialIndex = textInfo.characterInfo[i].materialReferenceIndex;
+            int vertexIndex = textInfo.characterInfo[i].vertexIndex;
 
-                palabraFade = palabras[palabraActual];
-                fadeTimer = 0;
-                haciendoFade = true;
-            }
+            Color32[] colors = textInfo.meshInfo[materialIndex].colors32;
+
+            colors[vertexIndex + 0].a = 0;
+            colors[vertexIndex + 1].a = 0;
+            colors[vertexIndex + 2].a = 0;
+            colors[vertexIndex + 3].a = 0;
         }
-        else
+
+        texto.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+
+        // Mostrar cada letra con fade
+        for (int i = 0; i < textInfo.characterCount; i++)
         {
-            fadeTimer += Time.deltaTime;
+            if (!textInfo.characterInfo[i].isVisible)
+                continue;
 
-            float t = Mathf.Clamp01(fadeTimer / duracionFade);
-            int alpha = Mathf.RoundToInt(t * 255);
-
-            string hexAlpha = alpha.ToString("X2");
-
-            texto.text =
-                textoFijo +
-                $"<color=#FFFFFF{hexAlpha}>{palabraFade}</color>";
-
-            if (fadeTimer >= duracionFade)
-            {
-                textoFijo += palabraFade + " ";
-                texto.text = textoFijo;
-
-                palabraActual++;
-                haciendoFade = false;
-            }
+            yield return StartCoroutine(FadeCaracter(i));
+            yield return new WaitForSeconds(tiempoEntreLetras);
         }
+
+        // Activar el objeto al terminar el diálogo
+        if (objetoAlFinal != null)
+            objetoAlFinal.SetActive(true);
+    }
+
+    IEnumerator FadeCaracter(int index)
+    {
+        TMP_TextInfo textInfo = texto.textInfo;
+
+        int materialIndex = textInfo.characterInfo[index].materialReferenceIndex;
+        int vertexIndex = textInfo.characterInfo[index].vertexIndex;
+
+        Color32[] colors = textInfo.meshInfo[materialIndex].colors32;
+
+        float t = 0;
+
+        while (t < duracionFade)
+        {
+            t += Time.deltaTime;
+            byte alpha = (byte)Mathf.Lerp(0, 255, t / duracionFade);
+
+            colors[vertexIndex + 0].a = alpha;
+            colors[vertexIndex + 1].a = alpha;
+            colors[vertexIndex + 2].a = alpha;
+            colors[vertexIndex + 3].a = alpha;
+
+            texto.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+
+            yield return null;
+        }
+
+        colors[vertexIndex + 0].a = 255;
+        colors[vertexIndex + 1].a = 255;
+        colors[vertexIndex + 2].a = 255;
+        colors[vertexIndex + 3].a = 255;
+
+        texto.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
     }
 }
